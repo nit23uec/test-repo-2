21ef2f330048d789dced556ce92ff3d910e142a8
Nitin Gupta <nitigupt@adobe.com>
2024-05-24 19:32:34 +0530

    fragment support

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -66,7 +66,7 @@ function annotateItems(items, formDefinition, formFieldMap) {
         console.warn(`field ${id} not found in form definition`);
       }
       if (fieldWrapper.classList.contains('panel-wrapper')) {
-        if (fd.properties['fd:fragment'] && document.documentElement.classList.contains('adobe-ue-edit')) {
+        if (fd.properties['fd:fragment']) {
           annotateFormFragment(fieldWrapper, fd);
         } else {
           fieldWrapper.setAttribute('data-aue-type', 'container');
