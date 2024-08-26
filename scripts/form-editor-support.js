5639a66b277cde53c230070e2116be97e151a427
Nitin Gupta <nitigupt@adobe.com>
2024-08-26 21:58:19 +0530

    update rule editor extension url

--- scripts/form-editor-support.js
+++ scripts/form-editor-support.js
@@ -333,7 +333,7 @@ function enableRuleEditorExtension() {
   let head = document.getElementsByTagName('head')[0];
   var meta = document.createElement('meta');
   meta.name = "urn:adobe:aue:config:extensions";
-  meta.content = "https://experience-stage.adobe.com/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html?livecycle-ruleeditor-ui-service_version=PR-25-1906e182c637890f9cecdbd5fddc5197e1533a8e";
+  meta.content = "https://experience-stage.adobe.com/solutions/livecycle-ruleeditor-ui-service/static-assets/resources/universal_editor.html";
   head.appendChild(meta);
 }
 
